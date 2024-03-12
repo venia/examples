1447bc7e9aaa707b13f40380fd53d10baad5968b
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-03-12 19:31:40 +0200

    [master] refactor basic

---
+++ react-and-spring-data-rest-example/basic/src/main/java/com/aquila/spring/data/example/basic/App.java
@@ -0,0 +1,13 @@
+package com.aquila.spring.data.example.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
