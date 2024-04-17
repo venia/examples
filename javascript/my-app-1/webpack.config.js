982f0356724847f2ae66620b10b92de78b252d7a
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-04-17 17:39:58 +0300

    [master] refactored project to use webpack

--- javascript/my-app-1/webpack.config.js
+++ javascript/my-app-1/webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+(() => {
+    console.log('webpack worked')
+})()
 module.exports = {
     entry: "./src/index.js", // Entry point of your application
+    mode: 'development',
     output: {
         filename: "bundle.js", // Output bundle file name
         path: path.resolve(__dirname, "dist"), // Output directory
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -29,26 +35,38 @@ module.exports = {
             }
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+    devServer: {
+        historyApiFallback: true
+    },
     resolve: {
         extensions: [".js", ".jsx"],
     },
-    devServer: {
-        //contentBase: path.join(__dirname, "public"),
-        static: {
-            directory: path.resolve(__dirname, "static"),
-            staticOptions: {},
-            // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
-            // Can be:
-            // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
-            publicPath: "/public/",
-            // Can be:
-            // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
-            serveIndex: true,
-            // Can be:
-            // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
-            watch: true,
-        },
-        port: 3000, // Port for the development server
-        open: true, // Open the default web browser when the server starts
-    },
+    // devServer: {
+    //     historyApiFallback: true,
+    //     static: {
+    //         directory: path.join(__dirname, 'dist')
+    //         //staticOptions: {},
+    //
+    //         // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+    //         // Can be:
+    //         // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+    //         //publicPath: "/public/",
+    //
+    //         // Can be:
+    //         // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+    //         //serveIndex: true,
+    //
+    //         // Can be:
+    //         // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+    //         //watch: true,
+    //     },
+    //     port: 3000, // Port for the development server
+    //     open: true, // Open the default web browser when the server starts
+    // },
 };
+
