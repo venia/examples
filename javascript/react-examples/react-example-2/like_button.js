7ca57773089a96ed5f187dbfe9a68afe6d756e99
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-04-26 19:11:24 +0300

    [master] babel stand alone

---
+++ javascript/react-examples/react-example-2/like_button.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom";
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+        <button onClick={() => this.setState({ liked: true })}>
+          Нравится
+        </button>
+    );
+  }
+}
