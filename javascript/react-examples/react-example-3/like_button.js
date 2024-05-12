1b6b4220dafdcaf9417eba5aef18b340550f61cb
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-05-12 17:08:54 +0300

    [master] configure 3-d example

--- javascript/react-examples/react-example-3/like_button.js
+++ javascript/react-examples/react-example-3/like_button.js
@@ -2,7 +2,7 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+export class LikeButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
@@ -21,6 +21,6 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
