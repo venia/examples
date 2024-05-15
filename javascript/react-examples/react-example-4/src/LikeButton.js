ea21b22ac4c43760f9cad64a9aecd1fa675c2474
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-05-15 12:03:57 +0300

    [master] change Like button

---
+++ javascript/react-examples/react-example-4/src/LikeButton.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+
+// const e = React.createElement;
+
+export class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    // return e(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   'Like'
+    // );
+
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>
+  }
+}
+
+export default LikeButton;
