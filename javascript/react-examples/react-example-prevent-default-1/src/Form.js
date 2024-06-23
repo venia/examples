48185e56549d537e75c0680f32d9cbe94f6fec02
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-06-23 17:12:30 +0300

    [master] added react-example-prevent-default-1

---
+++ javascript/react-examples/react-example-prevent-default-1/src/Form.js
@@ -0,0 +1,15 @@
+function Form() {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('You clicked submit.');
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <button type='submit'>Submit</button>
+        </form>
+    );
+}
+
+export default Form;
