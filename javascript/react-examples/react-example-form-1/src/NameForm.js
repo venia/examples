be983495d41fc1193df032fc446dbd584cb30734
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-07-07 14:18:14 +0300

    [master] added NameForm

---
+++ javascript/react-examples/react-example-form-1/src/NameForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Отправленное имя: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Имя:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Отправить" />
+            </form>
+        );
+    }
+}
