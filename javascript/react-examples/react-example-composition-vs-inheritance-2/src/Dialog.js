b3e7da36cb300f376b6b48fb063e6dc6f7b135d3
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-08-12 00:31:56 +0300

    [master] added new composition

---
+++ javascript/react-examples/react-example-composition-vs-inheritance-2/src/Dialog.js
@@ -0,0 +1,14 @@
+import FancyBorder from "./FancyBorder";
+
+export default function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+        </FancyBorder>
+    );
+}
---
+++ javascript/react-examples/react-example-composition-vs-inheritance-2/src/WelcomeDialog.js
@@ -0,0 +1,9 @@
+import Dialog from "./Dialog";
+
+export default function WelcomeDialog() {
+    return (
+        <Dialog
+            title="Добро пожаловать"
+            message="Спасибо, что посетили наш космический корабль!" />
+    );
+}
