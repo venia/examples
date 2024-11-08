771608892998e9093d1a2f70d532d858e06d594a
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-11-08 13:39:23 +0200

    [master] spring reactor first example

---
+++ java/spring-boot/spring-reactor-1/src/test/java/com/aquila/example/java/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquila.example.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
