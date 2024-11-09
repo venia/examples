a976946110ffe4283e332508e070dc139e463ba9
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-11-09 19:01:29 +0200

    [master] added react tests

--- java/spring-boot/spring-reactor-1/src/test/java/com/aquila/example/java/ReactTest.java
+++ java/spring-boot/spring-reactor-1/src/test/java/com/aquila/example/java/ReactTest.java
@@ -3,12 +3,64 @@ package com.aquila.example.java;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class ReactTest {
 
     @Test
-    void simpleFluxExample() {
+    void simpleFluxExample1() {
         Flux<String> fluxColors = Flux.just("red", "green", "blue");
         fluxColors.subscribe(System.out::println);
+    }
+
+    @Test
+    void simpleFluxExample2() {
+        Flux<String> fluxColors = Flux.just("red", "green", "blue");
+        fluxColors.log().subscribe(System.out::println);
+    }
+
+    @Test
+    void mapExample() {
+        Flux<String> fluxColors = Flux.just("red", "green", "blue");
+        fluxColors.map(color -> color.charAt(0)).subscribe(System.out::println);
+    }
+
+    @Test
+    void zipExample() {
+        Flux<String> fluxFruits = Flux.just("apple", "pear", "plum");
+        Flux<String> fluxColors = Flux.just("red", "green", "blue");
+        Flux<Integer> fluxAmounts = Flux.just(10, 20, 30);
+        Flux.zip(fluxFruits, fluxColors, fluxAmounts).subscribe(System.out::println);
+    }
+
+    @Test
+    public void onErrorExample() {
+        Flux<String> fluxCalc = Flux.just(-1, 0, 1)
+                .map(i -> "10 / " + i + " = " + (10 / i));
+
+        fluxCalc.subscribe(value -> System.out.println("Next: " + value),
+                error -> System.err.println("Error: " + error));
+    }
+
+    @Test
+    public void onErrorReturnExample() {
+        Flux<String> fluxCalc = Flux.just(-1, 0, 1)
+                .map(i -> "10 / " + i + " = " + (10 / i))
+                .onErrorReturn(ArithmeticException.class, "Division by 0 not allowed");
+
+        fluxCalc.subscribe(value -> System.out.println("Next: " + value),
+                error -> System.err.println("Error: " + error));
+
+    }
+
+    @Test
+    public void stepVerifierTest() {
+        Flux<String> fluxCalc = Flux.just(-1, 0, 1)
+                .map(i -> "10 / " + i + " = " + (10 / i));
+
+        StepVerifier.create(fluxCalc)
+                .expectNextCount(1)
+                .expectError(ArithmeticException.class)
+                .verify();
     }
 }
