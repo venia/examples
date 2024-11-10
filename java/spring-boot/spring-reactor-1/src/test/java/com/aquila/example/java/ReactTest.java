caaa64ddf355f16a2b615c2f032f2950f3c6bee8
v.zelenivskyi <v.zelenivskyi@alpha-serve.com>
2024-11-10 19:50:27 +0200

    [master] added new spring reactor tests

--- java/spring-boot/spring-reactor-1/src/test/java/com/aquila/example/java/ReactTest.java
+++ java/spring-boot/spring-reactor-1/src/test/java/com/aquila/example/java/ReactTest.java
@@ -2,8 +2,13 @@ package com.aquila.example.java;
 
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 class ReactTest {
 
@@ -62,5 +67,61 @@ class ReactTest {
                 .expectNextCount(1)
                 .expectError(ArithmeticException.class)
                 .verify();
+    }
+
+    @Test
+    public void publishSubscribeExample() {
+        Scheduler schedulerA = Schedulers.newParallel("Scheduler A");
+        Scheduler schedulerB = Schedulers.newParallel("Scheduler B");
+        Scheduler schedulerC = Schedulers.newParallel("Scheduler C");
+
+        Flux.just(1)
+                .map(i -> {
+                    System.out.println("First map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerA)
+                .map(i -> {
+                    System.out.println("Second map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerB)
+                .map(i -> {
+                    System.out.println("Third map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerC)
+                .map(i -> {
+                    System.out.println("Fourth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerA)
+                .map(i -> {
+                    System.out.println("Fifth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .blockLast();
+    }
+
+    @Test
+    public void coldPublisherExample() throws InterruptedException {
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1));
+        Thread.sleep(2000);
+        intervalFlux.subscribe(i -> System.out.println(String.format("Subscriber A, value: %d", i)));
+        Thread.sleep(2000);
+        intervalFlux.subscribe(i -> System.out.println(String.format("Subscriber B, value: %d", i)));
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void hotPublisherExample() throws InterruptedException {
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1));
+        ConnectableFlux<Long> intervalCF = intervalFlux.publish();
+        intervalCF.connect();
+        Thread.sleep(2000);
+        intervalCF.subscribe(i -> System.out.println(String.format("Subscriber A, value: %d", i)));
+        Thread.sleep(2000);
+        intervalCF.subscribe(i -> System.out.println(String.format("Subscriber B, value: %d", i)));
+        Thread.sleep(3000);
     }
 }
