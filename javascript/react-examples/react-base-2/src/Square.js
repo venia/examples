7362c1949422fce10216a4da1697667a3fe66a5f
veniamin.zelenivskyi <v.zelenivskyi@tempo.io>
2025-05-01 16:29:30 +0300

    [master] added Tic-tac-toe

--- javascript/react-examples/react-base-2/src/Square.js
+++ javascript/react-examples/react-base-2/src/Square.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import './Square.css';
 
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={this.props.onClick}>
                 {this.props.value}
             </button>
         );
     }
 }
+
+export default Square;
